21d478e9aea24df1cc299b7916a09b1e4a15afaf
prawish07 <prawish.lamichhane20@gmail.com>
2026-05-14 17:20:05 +0545

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Login from "./pages/Login";
 import PlaceOrder from "./pages/PlaceOrder";
 import Orders from "./pages/Orders";
 import Navrbar from "./components/Navrbar";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <div
@@ -28,6 +29,7 @@ const App = () => {
         <Route path="/place-order" element={<PlaceOrder />} />
         <Route path="orders" element={<Orders />} />
       </Routes>
+      <Footer/>
     </div>
   );
 };
